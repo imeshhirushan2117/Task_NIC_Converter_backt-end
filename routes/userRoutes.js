316ceb2492efb949c007cefbd9ab59a602c0f7d0
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-08-22 17:49:45 +0530

    MongoDB Connection Create

---
+++ routes/userRoutes.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const userController = require('../controllers/userController')
+
+const router = express.Router();
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 8080;
+const MONGODB_URL =
+  "mongodb+srv://admin:admin@nicconverter.abfmmtx.mongodb.net/nicConverter?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.error("Error connecting to MongoDB", err));
+app.use("/users", userRoutes);
